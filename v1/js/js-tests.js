372d44ed79514f6bdb41de2201e087018c182eed
Terrance Pitre <tpitre@southleft.com>
2015-03-18 17:39:27 -0500

    Adding intro slide functionality.

--- v1/js/js-tests.js
+++ v1/js/js-tests.js
@@ -3,22 +3,31 @@ var galleryPrev = '<span class="icon icon-arrow-left no-bg"><span class="is-vish
 var galleryNext = '<span class="icon icon-arrow-right no-bg"><span class="is-vishidden">Next</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" enable-background="new 0 0 500 500"><path d="M153.5 5.3l-31.7 31.6L334.9 250 121.8 463.1l31.7 31.6L398.2 250z"/></svg></span>';
 
 // Gallery init
-$('.js-slides').owlCarousel({
-  singleItem: true,
-  navigation: true,
-  pagination: false,
-  lazyLoad: true,
-  rewindNav: false,
-  slideSpeed: 0,
-  navigationText: [galleryPrev, galleryNext],
-  beforeMove: function() {
-  }
+var $slides = $('.js-slides');
+$('.btn--gallery-start').click(function(e) {
+  e.preventDefault();
+  // Hide starter slide.
+  $(this).parents('.slide-starter').hide();
+  // Load and show slides.
+  $slides.show().owlCarousel({
+    singleItem: true,
+    //navigation: true,
+    pagination: false,
+    lazyLoad: true,
+    rewindNav: false,
+    slideSpeed: 0,
+    navigationText: [galleryPrev, galleryNext],
+    beforeMove: function() {
+    }
+  });
 });
 
 // Carousel init
-$('.js-carousel').owlCarousel({
+var $carousel = $('.js-carousel');
+$carousel.show().owlCarousel({
   items: 8,
   lazyLoad: true,
   navigation: true,
+  rewindNav: false,
   navigationText: [galleryPrev, galleryNext]
 });
