91b8cd0732669d8a56604cccb656ab2fb63f49a7
Terrance Pitre <tpitre@southleft.com>
2015-03-18 22:49:30 -0500

    Working out the carousel and slides at all breakpoints.

--- source/js/js-tests.js
+++ source/js/js-tests.js
@@ -1,3 +1,8 @@
+var viewportWidth = $(window).width();
+$(window).resize(function() {
+  viewportWidth = $(window).width();
+  initCarousel();
+});
 
 var galleryPrev = '<span class="icon icon-arrow-left no-bg"><span class="is-vishidden">Previous</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" enable-background="new 0 0 500 500"><path d="M344.5 5.3l31.7 31.6L163.1 250l213.1 213.1-31.7 31.6L99.8 250z"/></svg></span>';
 var galleryNext = '<span class="icon icon-arrow-right no-bg"><span class="is-vishidden">Next</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" enable-background="new 0 0 500 500"><path d="M153.5 5.3l-31.7 31.6L334.9 250 121.8 463.1l31.7 31.6L398.2 250z"/></svg></span>';
@@ -20,19 +25,26 @@ $slides.show().owlCarousel({
 
 // Carousel init
 var $carousel = $('.js-carousel');
-$carousel.show().owlCarousel({
-  items: 8,
-  lazyLoad: true,
-  navigation: true,
-  rewindNav: false,
-  lazyEffect: false,
-  addClassActive: true,
-  mouseDrag: false,
-  navigationText: [galleryPrev, galleryNext],
-  afterInit: function(el){
-    el.find('.owl-item').eq(0).addClass('active-item');
+function initCarousel(){
+  if (viewportWidth >= 750) {
+    $carousel.show().owlCarousel({
+      items: 8,
+      lazyLoad: true,
+      navigation: true,
+      rewindNav: false,
+      lazyEffect: false,
+      addClassActive: true,
+      mouseDrag: false,
+      itemsCustom: [[0,8], [801,4], [900,5], [950,6], [1140,7], [1130,8]],
+      navigationText: [galleryPrev, galleryNext],
+      afterInit: function(el){
+        el.find('.owl-item').eq(0).addClass('active-item');
+      }
+    });
   }
-});
+}
+// Init
+initCarousel();
 
 // Function called after every slide advance.
 function afterAction(){
@@ -40,18 +52,21 @@ function afterAction(){
   // Update slide count.
   $('.slide-count').find('.slide-index').text(current);
 
+  // Update active item class.
   $('.js-carousel').find('.owl-item').removeClass('active-item').eq(current).addClass('active-item');
   if ($('.js-carousel').data('owlCarousel') !== undefined){
     centerCarousel(current)
   }
 }
 
+// Update the slide position.
 $('.js-carousel').on('click', '.owl-item', function(e){
   e.preventDefault();
   var number = $(this).data('owlItem');
   $slides.trigger('owl.goTo', number);
 });
 
+// Move the carousel depending on which slide is active.
 function centerCarousel(number){
   var carouselVisible = $carousel.data('owlCarousel').owl.visibleItems;
   var num = number;
