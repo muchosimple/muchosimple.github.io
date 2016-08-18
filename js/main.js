54486b3d70c7faab58661576604a32f640b86991
rohithpv23 <rohith_pathravalappil@mindtree.com>
2016-08-18 14:30:12 +0530

    RS-4416 - Cleaning up JS and CSS.

--- js/main.js
+++ js/main.js
@@ -112,25 +112,6 @@ window.onresize = function() {
     }
   });
 
-  // Toggle parent class - read-more
-  $('.action').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this),
-      // If the ancestor you want to toggle isn't the parent, set this data attr, on the child, to the class of the ancestor.
-      toggleParentClass = $this.attr('data-js-toggle-parent-class');
-    if (typeof toggleParentClass !== typeof undefined && toggleParentClass !== false) {
-      if (!$this.closest('.' + toggleParentClass).hasClass("has-more")) {
-        $this.closest('.' + toggleParentClass).addClass("has-more");
-      }
-      else {
-        $this.closest('.' + toggleParentClass).removeClass("has-more");
-      }
-      $this.closest('.' + toggleParentClass).toggleClass("has-less");
-    } else {
-      $this.parent().toggleClass('has-less');
-    }
-  });
-
   // Reset subnav when main menu is reopened.
   $('.nav-toggler').on('click', function() {
     $('.l-top').delay(300).queue(function() {
